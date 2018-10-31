d2b9bfb4f9b628501c750e0ca092d83609b0aded
Joeri Hermans <joeri@joerihermans.com>
2018-10-31 11:56:43 +0100

    Add code stubs

---
+++ project/code/setup.py
@@ -0,0 +1,39 @@
+"""```KeyChain``` setup file."""
+
+import os
+import re
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+exclusions=["doc", "experiments", "application"]
+packages = find_packages(exclude=exclusions)
+
+# Get the version string of cag.
+with open(os.path.join("cag", "__init__.py"), "rt") as fh:
+    _version = re.search(
+        '__version__\s*=\s*"(?P<version>.*)"\n',
+        fh.read()
+    ).group("version")
+
+# Module requirements.
+_install_requires = [
+    "numpy",
+    "flask",
+    "requists",
+    "hashlib"
+]
+
+_parameters = {
+    "install_requires": _install_requires,
+    "license": "BSD",
+    "name": "cag",
+    "packages": packages,
+    "platform": "any",
+    "url": "https://github.com/glouppe/info8002-large-scale-data-systems"
+    "version": _version
+}
+
+setup(**_parameters)
