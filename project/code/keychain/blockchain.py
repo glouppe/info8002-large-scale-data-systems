4c589a3c4cfdb91f3a751c486b54edc0d7c0cc8b
Joeri Hermans <joeri@joerihermans.com>
2018-11-05 15:04:32 +0100

    Add docs

--- project/code/keychain/blockchain.py
+++ project/code/keychain/blockchain.py
@@ -4,65 +4,39 @@ import multilib
 import numpy as np
 import requests
 
-from keychain.util import hash
-
 
 
 class Block:
 
-    def __init__(self, index, proof, transactions=[], previous_hash=None):
-        self._index = index
-        self._data = {}
-        for transaction in transactions:
-            self._data[transaction.key] = transaction.value
-        self._transactions = transactions
-        self._previoius_hash = previous_hash
-        self._proof = proof
-
-    def index(self):
-        return self._index
+    def __init__(self):
+        """Describe the properties of a block."""
+        raise NotImplementedError
 
     def proof(self):
-        return self._proof
+        """Return the proof of the current block."""
+        raise NotImplementedError
 
     def transactions(self):
-        return self._transactions
-
-    def transaction_hashes(self):
-        return [t.hash() for t in self._transactions]
-
-    def timestamp(self):
-        return self._timestamp
-
-    def hash(self):
-        return hash(self)
-
-    def keys():
-        return self._data.keys()
-
-    def value(self, key):
-        return self._data[key]
-
-    def __getitem__(self, key):
-        return self.value(key)
-
-    def is_valid(self):
+        """Returns the list of transactions associated with this block."""
         raise NotImplementedError
 
 
 class Transaction:
 
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-    def hash(self):
-        return hash(self)
+    def __init__(self):
+        """A transaction, in our KV setting. A transaction typically involves
+        some key, value and an origin (the one who put it onto the storage).
+        """
+        raise NotImplementedError
 
 
 class Peer:
 
     def __init__(self, address):
+        """Address of the peer.
+
+        Can be extended if desired.
+        """
         self._address = address
 
 
@@ -70,92 +44,44 @@ class Blockchain:
 
 
     def __init__(self, bootstrap, difficulty):
+        raise NotImplementedError
         # Initialize the properties.
         self._blocks = []
-        self._difficulty = difficulty
         self._peers = []
-        self._transactions = []
-        self._app = Flask()
-        self._initialize_rest()
+        self._difficulty = difficulty
         # Initialize the chain with the Genesis block.
         self._add_genesis_block()
         # Bootstrap the chain with the specified bootstrap address.
         self._bootstrap(bootstrap)
 
-    def _initialize_rest(self):
-        """REST Methods."""
-
-        # Mining call.
-        @self._app.route("/mine", methods=["GET"])
-        def mine():
-            raise NotImplementedError
-
-        # New transaction.
-        @self._app.route("/transaction", methods=["PUT"])
-        def new_transaction():
-            raise NotImplementedError
-
-        # Blockchain
-        @self._app.route("/blockchain", methods=["GET"])
-        def blockchain():
-            raise NotImplementedError
-
-        # Peers
-        @self._app.route("/peers", methods=["GET"])
-        def peers():
-            raise NotImplementedError
-
-        # Add peer.
-        @self._app.route("/peer", methods=["PUT"])
-        def add_peer():
-            raise NotImplementedError
-
-        # Remove peer.
-        @self._app.route("/peer", methods=["DELETE"])
-        def remove_peer():
-            raise NotImplementedError
-
 
     def _add_genesis_block(self):
-        genesis_transactions = []
-        genesis_transactions.append(
-            Transaction("baryogenesis", "https://en.wikipedia.org/wiki/Baryogenesis"),
-            Transaction("leptogenesis", "https://en.wikipedia.org/wiki/Leptogenesis_(physics)")
-            Transaction("lemaitre", "https://en.wikipedia.org/wiki/Georges_Lema%C3%AEtre"),
-            Transaction("hubble", "https://en.wikipedia.org/wiki/Edwin_Hubble")
-        )
-        genesis_proof = hash("whatever")
-        previous_hash = hash("emptiness")
-        genesis_block = Block(genesis_proof, genesis_transactions, previous_hash)
-        self._blocks.append(genesis_block)
+        """Adds the genesis block to your blockchain."""
+        raise NotImplementedError
 
     def _bootstrap(self, address):
+        """Implements the bootstrapping procedure."""
         peer = Peer(address)
         raise NotImplementedError
 
     def difficulty(self):
+        """Returns the difficulty level."""
         return self._difficulty
 
     def add_transaction(self, transaction):
+        """Adds a transaction to your current list of transactions,
+        and broadcasts it to your Blockchain network.
+        """
         raise NotImplementedError
 
-    def add_peer(self, peer):
-        self._peers.append(peer)
-
-    def remove_peer(self, peer):
-        self._peers.remove(peer)
+    def mine(self):
+        """Implements the mining procedure."""
+        raise NotImplementedError
 
     def is_valid(self):
+        """Checks if the current state of the blockchain is valid.
+
+        Meaning, are the sequence of hashes, and the proofs of the
+        blocks correct?
+        """
         raise NotImplementedError
-
-    def size(self):
-        return len(self._blocks)
-
-    def blocks(self):
-        return list(self._blocks)
-
-    def run(self):
-        self._app.run()
-
-    def __getitem__(self, index):
-        return self._blocks[index]
--- project/code/keychain/store.py
+++ project/code/keychain/store.py
@@ -12,25 +12,32 @@ class Callback:
         self._chain = chain
 
     def wait(self):
-        # Wait until the transaction appears in the blockchain.
+        """Wait until the transaction appears in the blockchain."""
         raise NotImplementedError
 
     def completed(self):
-        # Polls to blockchain to check if the data is available.
+        """Polls the blockchain to check if the data is available."""
         raise NotImplementedError
 
 
 class Storage:
 
-    def __init__(bootstrap, miner=False, difficulty=5)
-        # Allocate the blockchain storage.
-        self._blockchain = Blockchain(
-            bootstrap=bootstrap,
-            difficulty=difficulty
-        )
+    def __init__(self, bootstrap, miner, difficulty):
+        """Allocate the backend storage of the high level API, i.e.,
+        your blockchain. Depending whether or not the miner flag has
+        been specified, you should allocate the mining process.
+        """
+        self._blockchain = Blockchain()
+        raise NotImplementedError
 
     def put(self, key, value, block=True):
-        transaction = Transaction(key, value)
+        """Puts the specified key and value on the Blockchain.
+
+        The block flag specifies whether the call blocks until the value
+        has been put onto the blockchain, or if an error occurred.
+        """
+        raise NotImplementedError
+        transaction = Transaction(...)
         self._blockchain.add_transaction(self, transaction)
         callback = Callback(transaction, self._blockchain)
         if block:
@@ -39,7 +46,16 @@ class Storage:
         return callback
 
     def retrieve(self, key):
+        """Searches the most recent value of the specified key.
+
+        -> Search the list of blocks in reverse order for the specified key,
+        or implement some indexing schemes if you would like to do something
+        more efficient.
+        """
         raise NotImplementedError
 
     def retrieve_all(self, key):
+        """Retrieves all values associated with the specified key on the
+        complete blockchain.
+        """
         raise NotImplementedError
